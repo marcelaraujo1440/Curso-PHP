ec4a00dfb9114ecf0fa72048f788c8e8804d9f24
marcel araujo <marcelaraujo1440@gmail.com>
2024-08-15 19:08:57 -0300

    o

---
+++ exercicios.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Marmelad&family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+    <title>Curso PHP </title>
+    <link rel="stylesheet" href="/assets/style.css">
+</head>
+<body>
+   <header class="cabeçalho">
+        <h1>Curso PHP</h1>
+        <h2>Indice de Exercicios</h2>
+   </header>
+
+   <main class="principal">
+        <div class="conteudo">
+           <nav class="modulos">
+               <div class="modulo verde">
+                    <h3>modulo 01</h3>
+                    <ul>
+                         <li><a href="#">Exercicio A</a></li>
+                         <li><a href="#">Exercicio B</a></li>
+                         <li><a href="#">Exercicio C</a></li>
+                    </ul>
+               </div>
+               <div class="modulo vermelho">
+                    <h3>modulo 02</h3>
+                    <ul>
+                         <li><a href="#">Exercicio A</a></li>
+                         <li><a href="#">Exercicio B</a></li>
+                         <li><a href="#">Exercicio C</a></li>
+                    </ul>
+               </div><div class="modulo azul">
+                    <h3>modulo 03</h3>
+                    <ul>
+                         <li><a href="#">Exercicio A</a></li>
+                         <li><a href="#">Exercicio B</a></li>
+                         <li><a href="#">Exercicio C</a></li>
+                    </ul>
+               </div><div class="modulo roxo">
+                    <h3>modulo 04</h3>
+                    <ul>
+                         <li><a href="#">Exercicio A</a></li>
+                         <li><a href="#">Exercicio B</a></li>
+                         <li><a href="#">Exercicio C</a></li>
+                    </ul>
+               </div><div class="modulo laranja">
+                    <h3>modulo 05</h3>
+                    <ul>
+                         <li><a href="#">Exercicio A</a></li>
+                         <li><a href="#">Exercicio B</a></li>
+                         <li><a href="#">Exercicio C</a></li>
+                    </ul>
+               </div><div class="modulo verde-escuro">
+                    <h3>modulo 06</h3>
+                    <ul>
+                         <li><a href="#">Exercicio A</a></li>
+                         <li><a href="#">Exercicio B</a></li>
+                         <li><a href="#">Exercicio C</a></li>
+                    </ul>
+               </div><div class="modulo vermelho-escuro">
+                    <h3>modulo 07</h3>
+                    <ul>
+                         <li><a href="#">Exercicio A</a></li>
+                         <li><a href="#">Exercicio B</a></li>
+                         <li><a href="#">Exercicio C</a></li>
+                    </ul>
+               </div><div class="modulo azul-escuro">
+                    <h3>modulo 08</h3>
+                    <ul>
+                         <li><a href="#">Exercicio A</a></li>
+                         <li><a href="#">Exercicio B</a></li>
+                         <li><a href="#">Exercicio C</a></li>
+                    </ul>
+               </div>
+               <div class="modulo roxo-escuro">
+                    <h3>modulo 09</h3>
+                    <ul>
+                         <li><a href="#">Exercicio A</a></li>
+                         <li><a href="#">Exercicio B</a></li>
+                         <li><a href="#">Exercicio C</a></li>
+                    </ul>
+               </div>
+           </nav>
+        </div>
+   </main>
+   
+   <footer class="rodape">
+        COD3R & ALUNOS <?= date('Y');?>
+   </footer>
+</body>
+</html>
